7f6d84252d3433a3827fea00b212e69e859ad5c9
chaox <364270194@qq.com>
2023-06-15 01:14:56 +0800

    lab1 sleep

---
+++ user/sleep.c
@@ -0,0 +1,18 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+    int t;
+    if(argc < 2){
+        fprintf(2, "Usage: sleep seconds...\n");
+        exit(1);
+    }else{
+        t = atoi(argv[1]);
+        sleep(t);
+
+        exit(0);
+    }
+}
